e01f13671fc3fb84c0d3685ebbcf59b180907162
Hao <orctom@gmail.com>
2015-11-09 14:56:49 +0800

    Fixed multi-server mapping issue in cluster

--- src/main/resources/jython/websphere.py
+++ src/main/resources/jython/websphere.py
@@ -17,6 +17,7 @@ webModuleParentLast = r"{{webModuleParentLast}}"
 packageFile = r"{{packageFile}}"
 restartAfterDeploy = r"{{restartAfterDeploy}}"
 
+
 class WebSphere:
     def listApplications(self):
         print "[LIST APPLICATIONS]", host
@@ -24,18 +25,18 @@ class WebSphere:
         print AdminApp.list()
 
     def restartServer(self):
-        print '-'*60
+        print '-' * 60
         print "[RESTARTING SERVER]", host
         print time.strftime("%Y-%b-%d %H:%M:%S %Z")
-        print '-'*60
+        print '-' * 60
         try:
             if "" != cluster:
                 appManager = AdminControl.queryNames('name=' + cluster + ',type=Cluster,process=dmgr,*')
                 print AdminControl.invoke(appManager, 'rippleStart')
                 print ""
-                print '='*60
+                print '=' * 60
                 print "[NOTICE]: It takes a few minutes for the cluster to fully back running after the build finished!"
-                print '='*60
+                print '=' * 60
                 print ""
             elif "" != node:
                 appManager = AdminControl.queryNames('node=' + node + ',type=Server,process=' + server + ',*')
@@ -45,78 +46,97 @@ class WebSphere:
                 print AdminControl.invoke(appManager, 'restart')
         except:
             print "FAILED to restart cluster/server:"
-            print '-'*10
+            print '-' * 10
             traceback.print_exc(file=sys.stdout)
-            print '-'*10
+            print '-' * 10
             print "try to startApplication directly..."
             self.startApplication()
 
     def startApplication(self):
-        print '-'*60
+        print '-' * 60
         print "[STARTING APPLICATION]", host, applicationName
         print time.strftime("%Y-%b-%d %H:%M:%S %Z")
-        print '-'*60
+        print '-' * 60
         try:
             if "" != cluster:
-                appManager = AdminControl.queryNames('type=ApplicationManager,process=' + server + ',*')
+                cell = AdminControl.getCell()
+                managedNodeNames = AdminTask.listManagedNodes().splitlines()
+                for managedNodeName in managedNodeNames:
+                    appManager = AdminControl.queryNames(
+                        'type=ApplicationManager,process=' + server + ',node=' + managedNodeName + ',cell=' + cell + ',*')
+                    print "Starting " + applicationName + " on " + managedNodeName
             elif "" != node:
                 appManager = AdminControl.queryNames('node=' + node + ',type=Server,process=' + server + ',*')
             else:
                 appManager = AdminControl.queryNames('type=Server,process=' + server + ',*')
             print AdminControl.invoke(appManager, 'startApplication', applicationName)
-            #AdminApplication.startApplicationOnCluster(applicationName, cluster)
+            # AdminApplication.startApplicationOnCluster(applicationName, cluster)
         except:
             print "FAILED to start application:"
-            print '-'*10
+            print '-' * 10
             traceback.print_exc(file=sys.stdout)
-            print '-'*10
+            print '-' * 10
+            raise
 
     def stopApplication(self):
-        print '-'*60
+        print '-' * 60
         print "[STOPPING APPLICATION]", host, applicationName
         print time.strftime("%Y-%b-%d %H:%M:%S %Z")
-        print '-'*60
+        print '-' * 60
         try:
             if "" != cluster:
-                appManager = AdminControl.queryNames('type=ApplicationManager,process=' + server + ',*')
+                cell = AdminControl.getCell()
+                managedNodeNames = AdminTask.listManagedNodes().splitlines()
+                for managedNodeName in managedNodeNames:
+                    appManager = AdminControl.queryNames(
+                        'type=ApplicationManager,process=' + server + ',node=' + managedNodeName + ',cell=' + cell + ',*')
+                    print "Starting " + applicationName + " on " + managedNodeName
             elif "" != node:
                 appManager = AdminControl.queryNames('node=' + node + ',type=Server,process=' + server + ',*')
             else:
                 appManager = AdminControl.queryNames('type=Server,process=' + server + ',*')
             print AdminControl.invoke(appManager, 'stopApplication', applicationName)
-            #AdminApplication.stopApplicationOnCluster(applicationName, cluster)
+            # AdminApplication.stopApplicationOnCluster(applicationName, cluster)
         except:
             print "FAILED to stop application:"
-            print '-'*10
+            print '-' * 10
             traceback.print_exc(file=sys.stdout)
-            print '-'*10
+            print '-' * 10
+            raise
 
     def installApplication(self):
-        print '-'*60
+        print '-' * 60
         print "[INSTALLING APPLICATION]", host, applicationName
         print time.strftime("%Y-%b-%d %H:%M:%S %Z")
-        print '-'*60
+        print '-' * 60
 
-        options = ['-deployws', '-distributeApp', '-appname', applicationName, '-server', server]
+        options = ['-deployws', '-distributeApp', '-appname', applicationName]
 
         try:
             if "" != cluster:
-                serverMapping = 'WebSphere:cluster=' + cluster
-                options += ['-cluster', cluster, '-MapModulesToServers', [['.*','.*', serverMapping]]]
+                cell = AdminControl.getCell()
+                serverMapping = 'WebSphere:cell=' + cell + ',cluster=' + cluster
+                unmanagedNodeNames = AdminTask.listUnmanagedNodes().splitlines()
+                for unmanagedNodeName in unmanagedNodeNames:
+                    webservers = AdminTask.listServers('[-serverType WEB_SERVER -nodeName ' + unmanagedNodeName + ']').splitlines()
+                    for webserver in webservers:
+                        webserverName = AdminConfig.showAttribute(webserver, 'name')
+                        serverMapping = serverMapping + '+WebSphere:cell=' + cell + ',node=' + unmanagedNodeName + ',server=' + webserverName
+                options += ['-cluster', cluster, '-MapModulesToServers', [['.*', '.*', serverMapping]]]
             else:
                 serverMapping = 'WebSphere:server=' + server
-                options += ['-MapModulesToServers', [['.*','.*', serverMapping]]]
+                options += ['-MapModulesToServers', [['.*', '.*', serverMapping]]]
 
             if "" != contextRoot:
                 options += ['-contextroot', contextRoot]
 
             if "" != virtualHost:
-                options += ['-MapWebModToVH', [['.*','.*', virtualHost]]]
+                options += ['-MapWebModToVH', [['.*', '.*', virtualHost]]]
 
             if "" != sharedLibs:
                 libs = []
                 for lib in sharedLibs.split(','):
-                    libs.append(['.*','.*', lib])
+                    libs.append(['.*', '.*', lib])
                 options += ['-MapSharedLibForMod', libs]
 
             print ""
@@ -141,35 +161,34 @@ class WebSphere:
                 time.sleep(5)
                 result = AdminApp.isAppReady(applicationName)
             print "INSTALLED", applicationName
-            return "true"
         except:
             print "FAILED to install application: ", applicationName
-            print '-'*10
+            print '-' * 10
             traceback.print_exc(file=sys.stdout)
-            print '-'*10
-            return "false"
+            print '-' * 10
+            raise
 
     def __modifyClassloader(self):
         deployments = AdminConfig.getid("/Deployment:" + applicationName + "/")
         deploymentObject = AdminConfig.showAttribute(deployments, "deployedObject")
 
         if "true" == parentLast:
-            #AdminApplication.configureClassLoaderLoadingModeForAnApplication(applicationName, "PARENT_LAST")
+            # AdminApplication.configureClassLoaderLoadingModeForAnApplication(applicationName, "PARENT_LAST")
             classloader = AdminConfig.showAttribute(deploymentObject, "classloader")
             AdminConfig.modify(classloader, [['mode', 'PARENT_LAST']])
 
         if "true" == webModuleParentLast:
             modules = AdminConfig.showAttribute(deploymentObject, "modules")
-            arrayModules = modules[1:len(modules)-1].split(" ")
+            arrayModules = modules[1:len(modules) - 1].split(" ")
             for module in arrayModules:
                 if module.find('WebModuleDeployment') != -1:
                     AdminConfig.modify(module, [['classloaderMode', 'PARENT_LAST']])
 
     def uninstallApplication(self):
-        print '-'*60
+        print '-' * 60
         print "[UNINSTALLING APPLICATION]", host, applicationName
         print time.strftime("%Y-%b-%d %H:%M:%S %Z")
-        print '-'*60
+        print '-' * 60
         try:
             print AdminApp.uninstall(applicationName)
             AdminConfig.save()
@@ -177,9 +196,10 @@ class WebSphere:
                 AdminNodeManagement.syncActiveNodes()
         except:
             print "FAILED to uninstall application: ", applicationName
-            print '-'*10
+            print '-' * 10
             traceback.print_exc(file=sys.stdout)
-            print '-'*10
+            print '-' * 10
+            raise
 
     def isApplicationInstalled(self):
         return AdminApplication.checkIfAppExists(applicationName)
@@ -188,22 +208,21 @@ class WebSphere:
         if "true" == self.isApplicationInstalled():
             self.uninstallApplication()
 
-        if "true" == self.installApplication():
-            if "true" == restartAfterDeploy:
-                self.restartServer()
-            else:
-                print "Starting application instead of restarting server..."
-                self.startApplication()
+        self.installApplication()
+        if "true" == restartAfterDeploy:
+            self.restartServer()
+        else:
+            print "Starting application instead of restarting server..."
+            self.startApplication()
 
-        print '-'*60
+        print '-' * 60
         print "[FINISHED]", host, applicationName
         print time.strftime("%Y-%b-%d %H:%M:%S %Z")
-        print '-'*60
+        print '-' * 60
 
-
-#-----------------------------------------------------------------
+# -----------------------------------------------------------------
 # Main
-#-----------------------------------------------------------------
+# -----------------------------------------------------------------
 
 if __name__ == "__main__":
     methods, args = getopt.getopt(sys.argv, 'o:')
